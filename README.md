2de2ad92a6460038aced0357ebcfe4c73dc30f86
gcaceres93 <gabbaa011@gmail.com>
2017-07-16 00:24:26 -0400

    se borra todo

--- README.md
+++
@@ -1,2 +0,0 @@
-# psalud2
-Sistema de consultorio para psicología - Tesis 2017 Universidad Católica Ntra. Señora de la Asunción
--- resources/views/layouts/partials/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer>
-	<div class="footer navbar-fixed-bottom">
-		<center>@Copyright 2017 Universidad Catolica de Asuncion</center>
-	</div>
-</footer>
