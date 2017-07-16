c60ef5e02c622337a8ab56206b11da42eee4fbd3
gcaceres93 <gabbaa011@gmail.com>
2017-07-15 20:39:13 -0400

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,82 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::resource('ocupacion', 'OcupacionController');
+Route::get('/', 'HomeController@index')->name('home');
+
+// Route::get('tipo_terapia','TipoTerapiaController@index');
+// Route::resource('tipo_terapias','TipoTerapiaController');
+/* ************ Ejemplos sencillos ************** 
+Route::get('sucursal/{id}', function($id){
+	$sucursal = App\Sucursal::find($id);
+	return $sucursal->nombre;
+});
+
+Route::get('get_sucursal/{nombre}', function($nombre){
+	 $sucursal = App\Sucursal::where('nombre','=', $nombre)->first();
+
+	 echo $sucursal->nombre;
+});
+Route::get('movie', function(){
+	$movie = App\Movie::find(1);
+	print_r($movie);
+});
+
+Route::get('get_ocupacion/{id}', function($id){
+	$ocupacion = App\Ocupacion::find($id);
+	$personas = $ocupacion->personas;
+
+	echo "Personas con la ocupacion ".$ocupacion->nombre."<br />";
+	foreach ($personas as $persona) {
+		echo "Nombre y apellido: ".$persona->nombre." ".$persona->apellido."<br />";
+	}
+});
+
+Route::get('ocupacion', function(){
+	$ocupaciones = App\Ocupacion::all();
+	foreach ($ocupaciones as $ocupacion) {
+		echo  $ocupacion->nombre . ":<br />";
+	}
+}); 
+
+Route::get('persona', function(){
+	$personas = App\Persona::all();
+	foreach ($personas as $persona) {
+		echo $persona->nombre . " | Ocupacion:". $persona->ocupacion->nombre."<br />";
+	}
+});
+
+Route::get('users', ['uses' => 'UsersController@index']);
+Route::get('users/create', ['uses' => 'UsersController@create']);
+Route::post('users', ['uses' => 'UsersController@store']);
+
+/*
+Route::get('users',function() {
+	$users = [
+		'0' => [
+			'first_name' => 'Renato',
+			'last_name' => 'Hysa',
+			'location' => 'Albania'
+		],
+		'1' => [
+			'first_name' => 'Jessica',
+			'last_name' => 'Alba',
+			'location' => 'USA'
+		]
+	];
+	return $users;
+*/	
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
