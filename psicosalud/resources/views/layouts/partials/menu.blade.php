9543b4c7a6cbc3c80751f11d7b01d3ea6159ba7a
gcaceres93 <gabbaa011@gmail.com>
2017-07-18 01:10:35 -0400

    Se agrega registro de empleado al menu

--- psicosalud/resources/views/layouts/partials/menu.blade.php
+++ psicosalud/resources/views/layouts/partials/menu.blade.php
@@ -37,7 +37,7 @@
           <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pacientes <span class="glyphicon glyphicon-heart"></span></a>
               <ul class="dropdown-menu">
-                  <li><a href="#">Registrar paciente</a></li>
+                  <li><a href="{{ route('paciente.create') }}">Registrar paciente</a></li>
                   <li><a href="{{ url('/paciente') }}">Listar pacientes</a></li>
                   <li><a href="#">Historial de pacientes</a></li>
               </ul>
