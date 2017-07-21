9c99181b32199414a1cd67b4a24125d6cc3d565a
gcaceres93 <gabbaa011@gmail.com>
2017-07-21 04:57:44 -0400

    DatabaseSeeder

--- psicosalud/database/seeds/DatabaseSeeder.php
+++ psicosalud/database/seeds/DatabaseSeeder.php
@@ -11,12 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//         $this->call('UsersTableSeeder');
-//         $this->command->info("Se han cargado los datos a la tabla usuarios :)");
-//         $this->call('PersonasTableSeeder');
-//         $this->command->info("Se han cargado los datos a la tabla personas :)");
-//         $this->call('CargosTableSeeder');
-//         $this->command->info("Se han cargado los datos a la tabla cargos :)");
+        $this->call('UsersTableSeeder');
+        $this->command->info("Se han cargado los datos a la tabla usuarios :)");
+        $this->call('PersonasTableSeeder');
+        $this->command->info("Se han cargado los datos a la tabla personas :)");
+        $this->call('CargosTableSeeder');
+        $this->command->info("Se han cargado los datos a la tabla cargos :)");
         $this->call('SucursalesTableSeeder');
         $this->command->info("Se han cargado los datos a la tabla sucursales :)");
     }
