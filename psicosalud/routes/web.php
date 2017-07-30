2389e81156f1f8d7e14da6a97ce073ad4c4a2bcd
gcaceres93 <gabbaa011@gmail.com>
2017-07-30 02:22:32 -0400

    Route al reporte de asistencia

--- psicosalud/routes/web.php
+++ psicosalud/routes/web.php
@@ -26,7 +26,7 @@ Route::resource('agendamiento','AgendamientoController');
 Route::get('/verificarDisponibilidad','AgendamientoController@verificarDisponibilidad' );
 Route::get('/agendas','AgendamientoController@listarAgendas' );
 Route::get('/mostrarAgenda','AgendamientoController@mostrarAgenda' );
-Route::get('/reporteAsistencia','reporteAsistencia@reporteAsistenciax' );
+Route::get('/reporteAsistencia','reporteAsistencia@reporteAsistencia' );
 
 
 /*          PERSONAS          */
