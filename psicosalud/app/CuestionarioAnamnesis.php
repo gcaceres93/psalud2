598e24426aae2f1cdb6f96ef0ca123f300df10ad
gcaceres93 <gabbaa011@gmail.com>
2017-08-02 03:51:55 -0400

    Se agrega grupo y orden al cuestionarioanamnesisseeder

--- psicosalud/app/CuestionarioAnamnesis.php
+++ psicosalud/app/CuestionarioAnamnesis.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class CuestionarioAnamnesis extends Model
 {
-    //
+    protected $table = 'cuestionario_anamnesis';
+    public $timestamps = false;
+    
 }
